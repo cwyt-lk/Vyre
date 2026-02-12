6c0913ee892dcf476ee3e4c4d8694a2fb98a3c11
Connor Wythe <connor.wythe@gmail.com>
2026-02-12 05:46:39 -0800

    Setup with Supabase

---
+++ src/lib/supabase/server.ts
@@ -0,0 +1,27 @@
+/** biome-ignore-all lint/style/noNonNullAssertion: Supabase Docs */
+
+import "server-only";
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+import type { Database } from "@/types/supabase";
+
+export async function createClient() {
+  const cookieStore = await cookies();
+
+  return createServerClient<Database>(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return cookieStore.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) => {
+            cookieStore.set(name, value, options);
+          });
+        },
+      },
+    },
+  );
+}
