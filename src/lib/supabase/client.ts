2675a9e8dc93a7883f83f5876e9ad4ed4ea9f615
Connor Wythe <connor.wythe@gmail.com>
2026-02-13 01:51:54 -0800

    Fixed Supabase Libs

--- src/lib/supabase/client.ts
+++ src/lib/supabase/client.ts
@@ -1,12 +1,13 @@
 /** biome-ignore-all lint/style/noNonNullAssertion: Supabase Docs */
 
 import "client-only";
+
 import { createBrowserClient } from "@supabase/ssr";
 import type { Database } from "@/types/supabase";
 
 export function createClient() {
-  return createBrowserClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-  );
+	return createBrowserClient<Database>(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+	);
 }
--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,27 +1,32 @@
 /** biome-ignore-all lint/style/noNonNullAssertion: Supabase Docs */
 
 import "server-only";
+
 import { createServerClient } from "@supabase/ssr";
 import { cookies } from "next/headers";
 import type { Database } from "@/types/supabase";
 
 export async function createClient() {
-  const cookieStore = await cookies();
+	const cookieStore = await cookies();
 
-  return createServerClient<Database>(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
-    {
-      cookies: {
-        getAll() {
-          return cookieStore.getAll();
-        },
-        setAll(cookiesToSet) {
-          cookiesToSet.forEach(({ name, value, options }) => {
-            cookieStore.set(name, value, options);
-          });
-        },
-      },
-    },
-  );
+	return createServerClient<Database>(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY!,
+		{
+			cookies: {
+				getAll() {
+					return cookieStore.getAll();
+				},
+				setAll(cookiesToSet) {
+					try {
+						cookiesToSet.forEach(
+							({ name, value, options }) => {
+								cookieStore.set(name, value, options);
+							},
+						);
+					} catch {}
+				},
+			},
+		},
+	);
 }
