ac33af54182979538f588440ee9a23447371782f
Connor Wythe <connor.wythe@gmail.com>
2026-03-05 05:07:12 -0800

    Added capitalizeWords

--- src/lib/utils/string.ts
+++ src/lib/utils/string.ts
@@ -6,6 +6,10 @@ export function capitalize(str: string) {
 	return str.charAt(0).toUpperCase() + str.slice(1);
 }
 
+export function capitalizeWords(str: string) {
+	return str.split(" ").map(capitalize).join(" ");
+}
+
 export function slugify(text: string) {
 	return text
 		.toString()
