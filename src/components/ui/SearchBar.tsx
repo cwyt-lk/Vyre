67f8e448eaad25c5744505bb85fd26b3f5e942aa
Connor Wythe <connor.wythe@gmail.com>
2026-03-25 23:53:08 -0700

    Feature: Added a reusable SearchBar Component. Handles URL mutation client-side with debounce.

---
+++ src/components/ui/SearchBar.tsx
@@ -0,0 +1,65 @@
+"use client";
+
+import { Search } from "lucide-react";
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { type ChangeEvent, useState, useTransition } from "react";
+import { useDebouncedCallback } from "use-debounce";
+import {
+	InputGroup,
+	InputGroupAddon,
+	InputGroupInput,
+} from "@/components/ui/InputGroup";
+import { Spinner } from "@/components/ui/Spinner";
+
+export interface SearchBarProps {
+	placeholder?: string;
+}
+
+export const SearchBar = ({ placeholder }: SearchBarProps) => {
+	const searchParams = useSearchParams();
+	const pathname = usePathname();
+	const { replace } = useRouter();
+
+	const [isPending, startTransition] = useTransition();
+	const [search, setSearch] = useState(searchParams.get("query") ?? "");
+
+	const handleSearch = useDebouncedCallback((term: string) => {
+		const params = new URLSearchParams(searchParams);
+
+		if (term) {
+			params.set("query", term);
+		} else {
+			params.delete("query");
+		}
+
+		startTransition(() => {
+			replace(`${pathname}?${params.toString()}`, { scroll: false });
+		});
+	}, 400);
+
+	const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+		handleSearch(e.target.value);
+	};
+
+	return (
+		<InputGroup>
+			<InputGroupAddon>
+				{isPending ? (
+					<Spinner />
+				) : (
+					<Search className="text-muted-foreground" />
+				)}
+			</InputGroupAddon>
+
+			<InputGroupInput
+				value={search}
+				onChange={(e) => {
+					setSearch(e.target.value);
+					handleChange(e);
+				}}
+				placeholder={placeholder}
+				aria-label={placeholder}
+			/>
+		</InputGroup>
+	);
+};
