9be360fbecb84538fe3520773935c58fe64e124a
Connor Wythe <connor.wythe@gmail.com>
2026-03-25 23:50:56 -0700

    Feature: Added New RepoListResult Allows for repos to return an additional count data.

--- src/types/results/repo-result.ts
+++ src/types/results/repo-result.ts
@@ -2,3 +2,10 @@ import type { VyreError } from "@/types/errors";
 import type { OperationResult } from "@/types/results";
 
 export type RepoResult<T = void> = OperationResult<T, VyreError>;
+
+export type ListResult<T> = {
+	data: T[];
+	count: number;
+};
+
+export type RepoListResult<T> = RepoResult<ListResult<T>>;
